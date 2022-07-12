205cf4a288c62088eb54ef8cfc650b651dfd05bf
Raibann <84696229+raibann@users.noreply.github.com>
2022-07-12 17:42:02 +0700

    add table ui

--- src/pages/Expense/index.tsx
+++ src/pages/Expense/index.tsx
@@ -1,12 +1,125 @@
-import { Typography } from '@mui/material';
+import {
+  InputAdornment,
+  Pagination,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableContainer,
+  ToggleButtonGroup,
+} from '@mui/material';
+import CusTextField from 'components/CusTextField';
+import CusToggleButton from 'components/CusToggleButton';
 import PageHeader from 'components/PageHeader';
-import React from 'react';
+import { SearchNormal1 } from 'iconsax-react';
+import { ORDER_DATA } from 'pages/Orders';
+import { OrderTableHead, OrderTableBody } from 'pages/Orders/OrderTable';
+import { useState } from 'react';
+import theme from 'theme/theme';
 
 export default function Expense() {
+  const [ToggleValue, setToggleValue] = useState('pending');
   return (
     <>
       <PageHeader pageTitle={'Expense'} />
-      <Typography>gg</Typography>
+      <Paper
+        elevation={1}
+        sx={{
+          mx: 2,
+          borderRadius: 4,
+          minHeight: '92.5%',
+          maxWidth: '100%',
+          overflow: 'hidden',
+          px: 2,
+          position: 'relative',
+        }}
+      >
+        <Stack
+          direction='row'
+          justifyContent='space-between'
+          alignItems='center'
+          py={2}
+        >
+          <Stack
+            direction='row'
+            justifyContent='space-between'
+            alignItems='center'
+            sx={{ width: '100%' }}
+          >
+            <ToggleButtonGroup
+              value={ToggleValue}
+              exclusive
+              fullWidth
+              size='small'
+              onChange={(
+                event: React.MouseEvent<HTMLElement, MouseEvent>,
+                value: any
+              ) => {
+                if (value !== null) {
+                  setToggleValue(value);
+                }
+              }}
+              sx={{
+                width: '30%',
+              }}
+            >
+              <CusToggleButton value='pending'>Pending</CusToggleButton>
+              <CusToggleButton value='completed'>Completed</CusToggleButton>
+              <CusToggleButton value='all'>All</CusToggleButton>
+            </ToggleButtonGroup>
+            <CusTextField
+              placeholder='Search...'
+              size='small'
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <SearchNormal1
+                      size='20'
+                      color={theme.palette.primary.main}
+                    />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Stack>
+        </Stack>
+        <TableContainer>
+          <Table
+            stickyHeader
+            sx={{
+              '& thead tr th': {
+                background: (theme) => theme.palette.background.paper,
+                fontWeight: '600',
+              },
+            }}
+          >
+            <OrderTableHead />
+            <TableBody>
+              {ORDER_DATA.map((order) => {
+                return (
+                  <OrderTableBody
+                    key={order.id}
+                    id={order.id}
+                    name={order.name}
+                    social={order.social}
+                    bookingDate={order.bookingDate}
+                    deposit={order.deposit}
+                    eventDate={order.eventDate}
+                    eventLocation={order.eventLocation}
+                    paidBy={order.paidBy}
+                    quantity={order.quantity}
+                  />
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Pagination
+          count={10}
+          shape='rounded'
+          sx={{ position: 'absolute', bottom: 5 }}
+        />
+      </Paper>
     </>
   );
 }
