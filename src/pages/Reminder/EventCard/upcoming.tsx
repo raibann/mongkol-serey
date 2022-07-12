fdc968db93bc2fe23422c944a4517540933a78cc
Raibann <84696229+raibann@users.noreply.github.com>
2022-07-12 14:30:09 +0700

    update ui card reminder

--- src/pages/Reminder/EventCard/upcoming.tsx
+++ src/pages/Reminder/EventCard/upcoming.tsx
@@ -1,15 +1,6 @@
-import {
-  Paper,
-  Stack,
-  Typography,
-  Avatar,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Box,
-} from '@mui/material';
+import { Paper, Stack, Typography, Avatar, Box, Grid } from '@mui/material';
 import { Facebook, Gift, Call, Send2 } from 'iconsax-react';
+import theme from 'theme/theme';
 interface IUpcomingEvent {
   id: number;
   profile: string;
@@ -103,81 +94,85 @@ const UpcomingEvent = () => {
         sx={{
           display: 'flex',
           flexWrap: 'wrap',
-          rowGap: 2.5,
-          columnGap: 2.5,
+          rowGap: 3,
+          columnGap: 3,
         }}
       >
         {UpcomingEventData.map((data) => (
-          <Paper sx={{ width: 350, py: 2 }} key={data.id}>
-            <Stack
-              direction={'row'}
-              alignItems='center'
-              justifyContent={'space-between'}
-              px={2}
-            >
-              <Stack direction={'column'}>
+          <Paper sx={{ width: 310, py: 2, px: 1 }} key={data.id}>
+            <Grid container>
+              <Grid item xs={6}>
                 <Stack alignItems={'center'}>
                   <Avatar
                     src='/images/avatar.svg'
                     variant='square'
-                    sx={{ width: 100, height: 'auto' }}
+                    sx={{ width: 70, height: 'auto' }}
                   />
+                  <Stack>
+                    <Typography variant='h6'>{data.customerName}</Typography>
+                    <Typography
+                      variant='caption'
+                      sx={{ color: (theme) => theme.palette.text.secondary }}
+                    >
+                      Ordered: {data.ordered}
+                    </Typography>
+                    <Typography
+                      variant='caption'
+                      sx={{ color: (theme) => theme.palette.text.secondary }}
+                    >
+                      Last order: {data.lastOrder}
+                    </Typography>
+                  </Stack>
                 </Stack>
-
-                <Typography variant='h6'>{data.customerName}</Typography>
-                <Typography
-                  variant='caption'
-                  sx={{ color: (theme) => theme.palette.text.secondary }}
-                >
-                  Ordered: {data.ordered}
-                </Typography>
-                <Typography
-                  variant='caption'
-                  sx={{ color: (theme) => theme.palette.text.secondary }}
-                >
-                  Last order: {data.lastOrder}
-                </Typography>
-              </Stack>
-
-              <List dense={true} disablePadding>
-                <Stack direction={'row'} spacing={1}>
-                  <Typography variant='body1' fontWeight={'medium'}>
-                    Upcoming
-                  </Typography>
-                  <Typography
-                    variant='body1'
-                    fontWeight={'medium'}
-                    color={'error'}
-                  >
-                    {data.upComing}
-                  </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Stack justifyContent={'center'} spacing={2}>
+                  <Stack direction={'row'} spacing={1}>
+                    <Typography variant='body1' fontWeight={'medium'}>
+                      Upcoming
+                    </Typography>
+                    <Typography
+                      variant='body1'
+                      fontWeight={'medium'}
+                      color={'error'}
+                    >
+                      {data.upComing}
+                    </Typography>
+                  </Stack>
+                  <Stack>
+                    <Stack direction={'row'} spacing={1}>
+                      <Typography>
+                        <Facebook
+                          size='20'
+                          color={theme.palette.primary.main}
+                        />
+                      </Typography>
+                      <Typography variant='caption'>{data.facebook}</Typography>
+                    </Stack>
+                    <Stack direction={'row'} spacing={1}>
+                      <Typography>
+                        <Send2 size='20' color={theme.palette.primary.main} />
+                      </Typography>
+                      <Typography variant='caption'>{data.telegram}</Typography>
+                    </Stack>
+                    <Stack direction={'row'} spacing={1}>
+                      <Typography>
+                        <Call size='20' color={theme.palette.primary.main} />
+                      </Typography>
+                      <Typography variant='caption'>
+                        {data.contacNumber}
+                      </Typography>
+                    </Stack>
+                    <Stack direction={'row'} spacing={1}>
+                      <Typography>
+                        <Gift size='20' color={theme.palette.primary.main} />
+                      </Typography>
+                      <Typography variant='caption'>{data.gift}</Typography>
+                    </Stack>
+                  </Stack>
                 </Stack>
-                <ListItem>
-                  <ListItemIcon>
-                    <Facebook size='24' color='#FF8A65' />
-                  </ListItemIcon>
-                  <ListItemText>{data.facebook}</ListItemText>
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <Send2 size='24' color='#FF8A65' />
-                  </ListItemIcon>
-                  <ListItemText>{data.telegram}</ListItemText>
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <Call size='24' color='#FF8A65' />
-                  </ListItemIcon>
-                  <ListItemText>{data.contacNumber}</ListItemText>
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <Gift size='24' color='#FF8A65' />
-                  </ListItemIcon>
-                  <ListItemText> {data.gift}</ListItemText>
-                </ListItem>
-              </List>
-            </Stack>
+              </Grid>
+            </Grid>
           </Paper>
         ))}
       </Box>
