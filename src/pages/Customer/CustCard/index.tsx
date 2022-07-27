b48f2c9e9ac559e3d45a6d13a7a07c3929da5f93
mariabann <raibann.rb@gmail.com>
2022-07-27 09:37:05 +0700

    updated ui cutomer

--- src/pages/Customer/CustCard/index.tsx
+++ src/pages/Customer/CustCard/index.tsx
@@ -1,72 +1,75 @@
-import { Paper, Stack, Typography, Avatar, Box } from '@mui/material';
+import { Paper, Stack, Typography, Avatar, Grid } from '@mui/material';
 import { CusIconButton } from 'components/CusIconButton';
+import useRouter from 'hook/useRouter';
+
 import { Add, Edit, InfoCircle } from 'iconsax-react';
-import { ICustCard } from '..';
+import { ICustCard } from 'utils/cutomer-util';
 
 const CustCard = ({
   handleOpenDrawer,
-  CustCardData,
+  customerCard,
   setSelectedData,
 }: {
   handleOpenDrawer: (obj: 'Add' | 'Edit' | 'Details' | '') => void;
-  CustCardData: ICustCard[];
+  customerCard: ICustCard[];
   setSelectedData: React.Dispatch<React.SetStateAction<ICustCard[]>>;
 }) => {
+  const router = useRouter();
   return (
     <>
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          rowGap: 2.5,
-          columnGap: 2.8,
-        }}
-      >
-        {CustCardData.map((data) => (
-          <Paper sx={{ width: 200, py: 2 }} key={data.id}>
-            <Stack direction={'column'} alignItems='center' spacing={1.5}>
-              <Avatar
-                src='/images/avatar.svg'
-                sx={{ width: 100, height: 'auto' }}
-              />
-              <Stack alignItems={'center'}>
-                <Typography variant='h6'>{data.customerName}</Typography>
-                <Typography
-                  variant='caption'
-                  sx={{ color: (theme) => theme.palette.text.secondary }}
-                >
-                  Ordered: {data.ordered} times
-                </Typography>
+      <Grid container rowSpacing={2} columnSpacing={2}>
+        {customerCard.map((data, i) => (
+          <Grid item xs={6} md={2} key={i}>
+            <Paper sx={{ py: 2 }}>
+              <Stack direction={'column'} alignItems='center' spacing={1.5}>
+                <Avatar
+                  src='/images/avatar.svg'
+                  sx={{ width: 100, height: 'auto' }}
+                />
+                <Stack alignItems={'center'}>
+                  <Typography variant='h6'>{data.customerName}</Typography>
+                  <Typography
+                    variant='caption'
+                    sx={{ color: (theme) => theme.palette.text.secondary }}
+                  >
+                    Ordered: {data.ordered} times
+                  </Typography>
+                </Stack>
+                <Stack direction={'row'} spacing={1.5}>
+                  <CusIconButton
+                    color='primary'
+                    onClick={() => router.navigate('/orders')}
+                  >
+                    <Add variant='Outline' />
+                  </CusIconButton>
+                  <CusIconButton
+                    color='info'
+                    onClick={() => handleOpenDrawer('Edit')}
+                  >
+                    <Edit variant='Outline' />
+                  </CusIconButton>
+                  <CusIconButton
+                    color='success'
+                    onClick={(e) => {
+                      handleOpenDrawer('Details');
+                      if (e.target) {
+                        let temp = customerCard.filter(
+                          (el) => el.id === data.id
+                        );
+                        setSelectedData(temp);
+                      } else {
+                        setSelectedData(customerCard);
+                      }
+                    }}
+                  >
+                    <InfoCircle variant='Outline' />
+                  </CusIconButton>
+                </Stack>
               </Stack>
-              <Stack direction={'row'} spacing={1.5}>
-                <CusIconButton color='primary'>
-                  <Add variant='Outline' />
-                </CusIconButton>
-                <CusIconButton
-                  color='info'
-                  onClick={() => handleOpenDrawer('Edit')}
-                >
-                  <Edit variant='Outline' />
-                </CusIconButton>
-                <CusIconButton
-                  color='success'
-                  onClick={(e) => {
-                    handleOpenDrawer('Details');
-                    if (e.target) {
-                      let temp = CustCardData.filter((el) => el.id === data.id);
-                      setSelectedData(temp);
-                    } else {
-                      setSelectedData(CustCardData);
-                    }
-                  }}
-                >
-                  <InfoCircle variant='Outline' />
-                </CusIconButton>
-              </Stack>
-            </Stack>
-          </Paper>
+            </Paper>
+          </Grid>
         ))}
-      </Box>
+      </Grid>
     </>
   );
 };
--- src/pages/Customer/CustomerDetails.tsx
+++ src/pages/Customer/CustomerDetails.tsx
@@ -15,129 +15,174 @@ import {
   Call,
   Facebook,
   Gift,
-  Home3,
+  House,
   NotificationFavorite,
   Send2,
 } from 'iconsax-react';
 import theme from 'theme/theme';
-import { ICustCard } from '.';
+import { ICustCard } from 'utils/cutomer-util';
 
 export default function CustomerDetails({ data }: { data: ICustCard }) {
   return (
     <>
       <Grid container>
-        <Grid item xs={6}>
+        <Grid item xs={12}>
           <Stack alignItems={'center'}>
             <Avatar
               src='/images/avatar.svg'
-              sx={{ width: 150, height: 'auto' }}
+              sx={{ width: 100, height: 'auto' }}
             />
             <Typography variant='h6'>Customer Name</Typography>
           </Stack>
         </Grid>
-        <Grid item xs={6}>
-          {' '}
-          <List disablePadding>
-            <ListItem disablePadding>
+      </Grid>
+      <Divider />
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <List>
+            <ListItem>
               <ListItemIcon>
-                <Facebook size='24' color={theme.palette.primary.main} />
+                <Avatar
+                  sx={{
+                    background: (theme) => theme.palette.common.white,
+                    boxShadow: theme.shadows[1],
+                  }}
+                >
+                  <Facebook size='24' color={'#4267B2'} variant='Bold' />
+                </Avatar>
               </ListItemIcon>
-              <ListItemText color={theme.palette.text.secondary}>
-                {data.facebook}
-              </ListItemText>
+              <ListItemText primary={data.facebook} secondary={'Facebook'} />
             </ListItem>
-            <ListItem disablePadding>
+            <ListItem>
               <ListItemIcon>
-                <Send2 size='24' color={theme.palette.primary.main} />
+                <Avatar
+                  sx={{
+                    background: (theme) => theme.palette.common.white,
+                    boxShadow: theme.shadows[1],
+                  }}
+                >
+                  <Send2 size='24' color={'#229ED9'} variant='Bold' />
+                </Avatar>
               </ListItemIcon>
-              <ListItemText color={theme.palette.text.secondary}>
-                {data.telegram}
-              </ListItemText>
+              <ListItemText primary={data.telegram} secondary={'Telegram'} />
             </ListItem>
-            <ListItem disablePadding>
+            <ListItem>
               <ListItemIcon>
-                <Call size='24' color={theme.palette.primary.main} />
+                <Avatar
+                  sx={{
+                    background: (theme) => theme.palette.common.white,
+                    boxShadow: theme.shadows[1],
+                  }}
+                >
+                  <Call
+                    size='24'
+                    color={theme.palette.success.main}
+                    variant='Bold'
+                  />
+                </Avatar>
               </ListItemIcon>
-              <ListItemText color={theme.palette.text.secondary}>
-                {data.contact}
-              </ListItemText>
+              <ListItemText primary={data.contact} secondary={'Phone number'} />
             </ListItem>
-            <ListItem disablePadding>
-              <ListItemIcon sx={{ position: 'relative', top: 0 }}>
-                <Home3 size='24' color={theme.palette.primary.main} />
+            <ListItem>
+              <ListItemIcon>
+                <Avatar
+                  sx={{
+                    background: (theme) => theme.palette.common.white,
+                    boxShadow: theme.shadows[1],
+                  }}
+                >
+                  <House
+                    size='24'
+                    color={theme.palette.primary.main}
+                    variant='Bold'
+                  />
+                </Avatar>
               </ListItemIcon>
               <ListItemText
-                primary='Address'
-                secondary={`House No : ${data.houseNo}, street : ${data.stNo}, ${data.location}
+                primary={`House No : ${data.houseNo}, street : ${data.stNo}, ${data.location}
                ${data.province}, ${data.district}, ${data.commune}`}
+                secondary={'Address'}
               />
             </ListItem>
           </List>
         </Grid>
+        {/* right */}
+        <Grid item xs={12} md={6}>
+          <List>
+            <ListItem>
+              <ListItemIcon>
+                <Avatar
+                  sx={{
+                    background: (theme) => theme.palette.common.white,
+                    boxShadow: theme.shadows[1],
+                  }}
+                >
+                  <BagTick2
+                    size='24'
+                    color={theme.palette.success.main}
+                    variant='Bold'
+                  />
+                </Avatar>
+              </ListItemIcon>
+              <ListItemText
+                primary='Ordered'
+                secondary={`${data.ordered}times`}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <Avatar
+                  sx={{
+                    background: (theme) => theme.palette.common.white,
+                    boxShadow: theme.shadows[1],
+                  }}
+                >
+                  <BagTimer
+                    size='24'
+                    color={theme.palette.info.main}
+                    variant='Bold'
+                  />
+                </Avatar>
+              </ListItemIcon>
+              <ListItemText primary='Last Ordered' secondary={data.lastOrder} />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <Avatar
+                  sx={{
+                    background: (theme) => theme.palette.common.white,
+                    boxShadow: theme.shadows[1],
+                  }}
+                >
+                  <NotificationFavorite
+                    size='24'
+                    color={theme.palette.warning.main}
+                    variant='Bold'
+                  />
+                </Avatar>
+              </ListItemIcon>
+              <ListItemText primary='Upcoming' secondary={data.upComing} />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <Avatar
+                  sx={{
+                    background: (theme) => theme.palette.common.white,
+                    boxShadow: theme.shadows[1],
+                  }}
+                >
+                  <Gift
+                    size='24'
+                    color={theme.palette.primary.main}
+                    variant='Bold'
+                  />
+                </Avatar>
+              </ListItemIcon>
+              <ListItemText primary='Gifts' secondary={data.gift} />
+            </ListItem>
+          </List>
+        </Grid>
       </Grid>
-      <Divider />
-      <Stack>
-        <List>
-          <ListItem>
-            <ListItemIcon>
-              <Avatar
-                sx={{
-                  background: (theme) => theme.palette.common.white,
-                  boxShadow: theme.shadows[1],
-                }}
-              >
-                <BagTick2 size='24' color={theme.palette.success.main} />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText
-              primary='Ordered'
-              secondary={`${data.ordered}times`}
-            />
-          </ListItem>
-          <ListItem>
-            <ListItemIcon>
-              <Avatar
-                sx={{
-                  background: (theme) => theme.palette.common.white,
-                  boxShadow: theme.shadows[1],
-                }}
-              >
-                <BagTimer size='24' color={theme.palette.info.main} />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText primary='Last Ordered' secondary={data.lastOrder} />
-          </ListItem>
-          <ListItem>
-            <ListItemIcon>
-              <Avatar
-                sx={{
-                  background: (theme) => theme.palette.common.white,
-                  boxShadow: theme.shadows[1],
-                }}
-              >
-                <NotificationFavorite
-                  size='24'
-                  color={theme.palette.warning.main}
-                />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText primary='Upcoming' secondary={data.upComing} />
-          </ListItem>
-          <ListItem>
-            <ListItemIcon>
-              <Avatar
-                sx={{
-                  background: (theme) => theme.palette.common.white,
-                  boxShadow: theme.shadows[1],
-                }}
-              >
-                <Gift size='24' color={theme.palette.primary.main} />
-              </Avatar>
-            </ListItemIcon>
-            <ListItemText primary='Gifts' secondary={data.gift} />
-          </ListItem>
-        </List>
-      </Stack>
     </>
   );
 }
