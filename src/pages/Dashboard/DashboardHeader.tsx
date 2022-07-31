92ca7668b71ca0bc1f9fa00a7d9f9f90b72d7fab
raibann <raibann.rb@gmail.com>
2022-07-31 16:51:13 +0700

    add date filter

--- src/pages/Dashboard/DashboardHeader.tsx
+++ src/pages/Dashboard/DashboardHeader.tsx
@@ -2,9 +2,11 @@ import {
   List,
   ListSubheader,
   Menu,
+  Popover,
   Stack,
+  TextField,
   ToggleButtonGroup,
-  Typography,
+  Typography
 } from '@mui/material';
 import { useState } from 'react';
 import theme from 'theme/theme';
@@ -15,22 +17,48 @@ import { CusIconButton } from 'components/CusIconButton';
 import { Calendar2 } from 'iconsax-react';
 import { Notification } from 'iconsax-react';
 import AnniversaryItem from './AnniversaryItem';
-
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import moment from 'moment';
 const DashboardHeader = () => {
   const [ToggleValue, setToggleValue] = useState('week');
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
+  const [datePicked, setDatePicked] = React.useState<Date | null>(new Date());
+  // ancher notification
+  const [
+    anchorElNotification,
+    setAnchorElNotification
+  ] = React.useState<null | HTMLElement>(null);
+  // anchor datepicker
+  const [
+    anchorElDatePicker,
+    setAnchorElDatePicker
+  ] = React.useState<null | HTMLElement>(null);
+  // notification
+  const handleClickNoti = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElNotification(event.currentTarget);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
+  // datepicker
+
+  const handleClickDatepicker = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setAnchorElDatePicker(event.currentTarget);
+  };
+  // handle close
+  const handleClose = (type: string) => {
+    if (type === 'notification') {
+      setAnchorElNotification(null);
+    } else if (type === 'datepicker') {
+      setAnchorElDatePicker(null);
+    }
   };
   const Anniversary = Array(6).fill({
     name: 'Meas Saominea',
     lastOrder: '30-02-2002',
-    daysLeft: 0,
+    daysLeft: 0
   });
+  console.log('datePicked:', moment(datePicked).format('DD-MM-YYYY'));
   return (
     <>
       <PageHeader
@@ -39,7 +67,7 @@ const DashboardHeader = () => {
           <>
             <CusIconButton
               color='primary'
-              onClick={handleClick}
+              onClick={handleClickNoti}
               sx={{ height: 40 }}
             >
               <Notification size='24' variant='Bold' />
@@ -69,7 +97,7 @@ const DashboardHeader = () => {
             }}
             sx={{
               width: { xs: '100%', md: '30%' },
-              height: 40,
+              height: 40
             }}
           >
             <CusToggleButton value='week'>Week</CusToggleButton>
@@ -77,19 +105,19 @@ const DashboardHeader = () => {
             <CusToggleButton value='year'>Year</CusToggleButton>
           </ToggleButtonGroup>
 
-          <CusIconButton color='primary'>
+          <CusIconButton color='primary' onClick={handleClickDatepicker}>
             <Calendar2 size='24' variant='Outline' />
           </CusIconButton>
 
           <CusIconButton
             color='primary'
-            onClick={handleClick}
+            onClick={handleClickNoti}
             sx={{
               display: {
                 xs: 'none',
-                md: 'block',
+                md: 'block'
               },
-              height: 40,
+              height: 40
             }}
           >
             <Notification size='24' variant='Bold' />
@@ -100,25 +128,25 @@ const DashboardHeader = () => {
         MenuListProps={{ sx: { p: 0 } }}
         sx={{
           mt: 1,
-          height: 350,
+          height: 350
         }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
+        anchorEl={anchorElNotification}
+        open={Boolean(anchorElNotification)}
+        onClose={() => handleClose('notification')}
       >
         <List
           disablePadding
           sx={{
             width: 350,
             px: 2,
-            pb: 2,
+            pb: 2
           }}
         >
           <ListSubheader
             sx={{
               p: 1,
               px: 2,
-              background: (theme) => theme.palette.common.white,
+              background: theme => theme.palette.common.white
             }}
           >
             <Typography
@@ -138,6 +166,36 @@ const DashboardHeader = () => {
           ))}
         </List>
       </Menu>
+      {/* pop over */}
+      <Popover
+        id={Boolean(anchorElDatePicker) ? 'simple-popover' : undefined}
+        open={Boolean(anchorElDatePicker)}
+        anchorEl={anchorElDatePicker}
+        onClose={() => handleClose('datepicker')}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
+        }}
+      >
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <StaticDatePicker
+            displayStaticWrapperAs='desktop'
+            openTo='year'
+            value={datePicked}
+            onChange={newValue => {
+              setDatePicked(newValue);
+              if (newValue?.getDate() !== datePicked?.getDate()) {
+                setAnchorElDatePicker(null);
+              }
+            }}
+            renderInput={params => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+      </Popover>
     </>
   );
 };
