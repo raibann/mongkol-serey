f73773d33ab7f3e48e38b90d2b767bf050fc861c
raibann <raibann.rb@gmail.com>
2022-08-20 14:08:44 +0700

    update date range pikcer

--- src/pages/Dashboard/DashboardHeader.tsx
+++ src/pages/Dashboard/DashboardHeader.tsx
@@ -1,12 +1,12 @@
 import {
+  Button,
   List,
   ListSubheader,
   Menu,
   Popover,
   Stack,
-  TextField,
   ToggleButtonGroup,
-  Typography
+  Typography,
 } from '@mui/material';
 import { useState } from 'react';
 import theme from 'theme/theme';
@@ -19,27 +19,40 @@ import { Notification } from 'iconsax-react';
 import AnniversaryItem from './AnniversaryItem';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+// import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 import moment from 'moment';
+// import ResponsiveDialog from 'components/CusDialog/ResponsiveDialog';
+import { DatePicker } from '@mui/x-date-pickers';
+import CusTextField from 'components/CusTextField';
+import LabelTextField from 'components/LabelTextField';
+import useResponsive from 'hook/useResponsive';
+import { Controller, useForm } from 'react-hook-form';
+
+interface IDateRange {
+  startDate: string;
+  endDate: string;
+}
+
 const DashboardHeader = () => {
   const [ToggleValue, setToggleValue] = useState('week');
-  const [datePicked, setDatePicked] = React.useState<Date | null>(new Date());
   // ancher notification
-  const [
-    anchorElNotification,
-    setAnchorElNotification
-  ] = React.useState<null | HTMLElement>(null);
+  const [anchorElNotification, setAnchorElNotification] =
+    React.useState<null | HTMLElement>(null);
   // anchor datepicker
-  const [
-    anchorElDatePicker,
-    setAnchorElDatePicker
-  ] = React.useState<null | HTMLElement>(null);
+  const [anchorElDatePicker, setAnchorElDatePicker] =
+    React.useState<null | HTMLElement>(null);
+
+  // Validate date range
+  const { handleSubmit, control } = useForm<IDateRange>();
+  const handleSubmitDate = (data: IDateRange) => {
+    console.log('start date', moment(data.startDate).format('DD-MM-YYYY'));
+    console.log('end date', moment(data.endDate).format('DD-MM-YYYY'));
+  };
   // notification
   const handleClickNoti = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorElNotification(event.currentTarget);
   };
   // datepicker
-
   const handleClickDatepicker = (
     event: React.MouseEvent<HTMLButtonElement>
   ) => {
@@ -56,9 +69,9 @@ const DashboardHeader = () => {
   const Anniversary = Array(6).fill({
     name: 'Meas Saominea',
     lastOrder: '30-02-2002',
-    daysLeft: 0
+    daysLeft: 0,
   });
-  console.log('datePicked:', moment(datePicked).format('DD-MM-YYYY'));
+  const { isSmDown } = useResponsive();
   return (
     <>
       <PageHeader
@@ -97,7 +110,7 @@ const DashboardHeader = () => {
             }}
             sx={{
               width: { xs: '100%', md: '30%' },
-              height: 40
+              height: 40,
             }}
           >
             <CusToggleButton value='week'>Week</CusToggleButton>
@@ -115,9 +128,9 @@ const DashboardHeader = () => {
             sx={{
               display: {
                 xs: 'none',
-                md: 'block'
+                md: 'block',
               },
-              height: 40
+              height: 40,
             }}
           >
             <Notification size='24' variant='Bold' />
@@ -128,7 +141,7 @@ const DashboardHeader = () => {
         MenuListProps={{ sx: { p: 0 } }}
         sx={{
           mt: 1,
-          height: 350
+          height: 350,
         }}
         anchorEl={anchorElNotification}
         open={Boolean(anchorElNotification)}
@@ -139,14 +152,14 @@ const DashboardHeader = () => {
           sx={{
             width: 350,
             px: 2,
-            pb: 2
+            pb: 2,
           }}
         >
           <ListSubheader
             sx={{
               p: 1,
               px: 2,
-              background: theme => theme.palette.common.white
+              background: (theme) => theme.palette.common.white,
             }}
           >
             <Typography
@@ -174,27 +187,73 @@ const DashboardHeader = () => {
         onClose={() => handleClose('datepicker')}
         anchorOrigin={{
           vertical: 'bottom',
-          horizontal: 'center'
+          horizontal: 'right',
         }}
         transformOrigin={{
           vertical: 'top',
-          horizontal: 'center'
+          horizontal: 'right',
         }}
+        sx={{ mt: 1 }}
       >
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <StaticDatePicker
-            displayStaticWrapperAs='desktop'
-            openTo='year'
-            value={datePicked}
-            onChange={newValue => {
-              setDatePicked(newValue);
-              if (newValue?.getDate() !== datePicked?.getDate()) {
-                setAnchorElDatePicker(null);
-              }
-            }}
-            renderInput={params => <TextField {...params} />}
-          />
-        </LocalizationProvider>
+        <form
+          style={{ height: '100%' }}
+          onSubmit={handleSubmit(handleSubmitDate)}
+        >
+          <Stack sx={{ p: 2, height: '100%' }} spacing={2}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <Stack
+                direction={isSmDown ? 'column' : 'row'}
+                spacing={2}
+                // alignItems='center'
+              >
+                <Controller
+                  control={control}
+                  name='startDate'
+                  render={({ field }) => (
+                    <LabelTextField label='Start date'>
+                      <DatePicker
+                        openTo='day'
+                        views={['year', 'month', 'day']}
+                        value={field.value}
+                        onChange={(date) => {
+                          field.onChange(date);
+                        }}
+                        renderInput={(params) => <CusTextField {...params} />}
+                      />
+                    </LabelTextField>
+                  )}
+                />
+                <Controller
+                  control={control}
+                  name='endDate'
+                  render={({ field }) => (
+                    <LabelTextField label='End date'>
+                      <DatePicker
+                        openTo='day'
+                        views={['year', 'month', 'day']}
+                        value={field.value}
+                        onChange={(date) => {
+                          field.onChange(date);
+                        }}
+                        renderInput={(params) => <CusTextField {...params} />}
+                      />
+                    </LabelTextField>
+                  )}
+                />
+              </Stack>
+            </LocalizationProvider>
+            <Button
+              variant='contained'
+              type='submit'
+              onClick={() => handleClose('datepicker')}
+              sx={{
+                width: '100%',
+              }}
+            >
+              Confirm
+            </Button>
+          </Stack>
+        </form>
       </Popover>
     </>
   );
