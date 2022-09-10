77afdbd00349198ae4d114f1725843e4c5d4629a
raibann <raibann.rb@gmail.com>
2022-09-10 15:00:19 +0700

    connected all api for dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -12,7 +12,12 @@ import DASHBOARD_API from 'api/dashboard';
 import { CusLoading } from 'components/CusLoading';
 import DashboardCard from 'components/DashboardCard';
 import useResponsive from 'hook/useResponsive';
-import { DollarCircle, Profile2User, WalletAdd } from 'iconsax-react';
+import {
+  BoxRemove,
+  DollarCircle,
+  Profile2User,
+  WalletAdd,
+} from 'iconsax-react';
 import moment from 'moment';
 import { useEffect, useState } from 'react';
 import {
@@ -29,126 +34,125 @@ import theme from 'theme/theme';
 import AnniversaryItem from './AnniversaryItem';
 import DashboardHeader from './DashboardHeader';
 import PieChartComp from './PieChartComp';
+// const CHART1_DATA = [
+//   {
+//     name: 'Jan',
+//     Events: 15,
+//   },
+//   {
+//     name: 'Feb',
+//     Events: 30,
+//   },
+//   {
+//     name: 'Mar',
+//     Events: 20,
+//   },
+//   {
+//     name: 'Apr',
+//     Events: 27,
+//   },
+//   {
+//     name: 'May',
+//     Events: 18,
+//   },
+//   {
+//     name: 'Jun',
+//     Events: 23,
+//   },
+//   {
+//     name: 'Jul',
+//     Events: 34,
+//   },
+//   {
+//     name: 'Aug',
+//     Events: 10,
+//   },
+//   {
+//     name: 'Sep',
+//     Events: 28,
+//   },
+//   {
+//     name: 'Oct',
+//     Events: 34,
+//   },
+//   {
+//     name: 'Nov',
+//     Events: 23,
+//   },
+//   {
+//     name: 'Dec',
+//     Events: 16,
+//   },
+// ];
 
-const CHART1_DATA = [
-  {
-    name: 'Jan',
-    Events: 15,
-  },
-  {
-    name: 'Feb',
-    Events: 30,
-  },
-  {
-    name: 'Mar',
-    Events: 20,
-  },
-  {
-    name: 'Apr',
-    Events: 27,
-  },
-  {
-    name: 'May',
-    Events: 18,
-  },
-  {
-    name: 'Jun',
-    Events: 23,
-  },
-  {
-    name: 'Jul',
-    Events: 34,
-  },
-  {
-    name: 'Aug',
-    Events: 10,
-  },
-  {
-    name: 'Sep',
-    Events: 28,
-  },
-  {
-    name: 'Oct',
-    Events: 34,
-  },
-  {
-    name: 'Nov',
-    Events: 23,
-  },
-  {
-    name: 'Dec',
-    Events: 16,
-  },
-];
+// const CHART2_DATA = [
+//   {
+//     name: 'Jan',
+//     Sales: 27,
+//     Expenses: 10,
+//   },
+//   {
+//     name: 'Feb',
+//     Sales: 30,
+//     Expenses: 20,
+//   },
+//   {
+//     name: 'Mar',
+//     Sales: 20,
+//     Expenses: 10,
+//   },
+//   {
+//     name: 'Apr',
+//     Sales: 27,
+//     Expenses: 17,
+//   },
+//   {
+//     name: 'May',
+//     Sales: 18,
+//     Expenses: 8,
+//   },
+//   {
+//     name: 'Jun',
+//     Sales: 23,
+//     Expenses: 15,
+//   },
+//   {
+//     name: 'Jul',
+//     Sales: 34,
+//     Expenses: 13,
+//   },
+//   {
+//     name: 'Aug',
+//     Sales: 10,
+//     Expenses: 4,
+//   },
+//   {
+//     name: 'Sep',
+//     Sales: 28,
+//     Expenses: 7,
+//   },
+//   {
+//     name: 'Oct',
+//     Sales: 34,
+//     Expenses: 22,
+//   },
+//   {
+//     name: 'Nov',
+//     Sales: 23,
+//     Expenses: 20,
+//   },
+//   {
+//     name: 'Dec',
+//     Sales: 16,
+//     Expenses: 10,
+//   },
+// ];
 
-const CHART2_DATA = [
-  {
-    name: 'Jan',
-    Sales: 27,
-    Expenses: 10,
-  },
-  {
-    name: 'Feb',
-    Sales: 30,
-    Expenses: 20,
-  },
-  {
-    name: 'Mar',
-    Sales: 20,
-    Expenses: 10,
-  },
-  {
-    name: 'Apr',
-    Sales: 27,
-    Expenses: 17,
-  },
-  {
-    name: 'May',
-    Sales: 18,
-    Expenses: 8,
-  },
-  {
-    name: 'Jun',
-    Sales: 23,
-    Expenses: 15,
-  },
-  {
-    name: 'Jul',
-    Sales: 34,
-    Expenses: 13,
-  },
-  {
-    name: 'Aug',
-    Sales: 10,
-    Expenses: 4,
-  },
-  {
-    name: 'Sep',
-    Sales: 28,
-    Expenses: 7,
-  },
-  {
-    name: 'Oct',
-    Sales: 34,
-    Expenses: 22,
-  },
-  {
-    name: 'Nov',
-    Sales: 23,
-    Expenses: 20,
-  },
-  {
-    name: 'Dec',
-    Sales: 16,
-    Expenses: 10,
-  },
-];
-
-const Anniversary = Array(6).fill({
-  name: 'Meas Saominea',
-  lastOrder: '30-02-2002',
-  daysLeft: 3,
-});
+// const Anniversary = Array(6).fill({
+//   name: 'Meas Saominea',
+//   lastOrder: '30-02-2002',
+//   daysLeft: 3,
+// });
 
 const Dashboard = () => {
   // use moment
@@ -159,6 +163,7 @@ const Dashboard = () => {
     endDate: moment(friday).format('YYYY-MM-DD'),
   });
   const { isMdDown } = useResponsive();
+  // fetch total
   const {
     data: dashTotal,
     run: fetchDashTotal,
@@ -166,7 +171,12 @@ const Dashboard = () => {
   } = useRequest(DASHBOARD_API.getTotals, {
     manual: true,
   });
-  console.log(dateRange);
+  // fetch chart
+  const { data: chartData, loading: isLoadingChart } = useRequest(
+    DASHBOARD_API.getCharts,
+    { manual: false }
+  );
+  // useEffect total
   useEffect(() => {
     fetchDashTotal({
       startDate: dateRange.startDate,
@@ -174,8 +184,61 @@ const Dashboard = () => {
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dateRange]);
-  // console.log('test', dashTotal);
+  // console.log(dateRange);
+  // function generate month
+  const monthGenerate = (value: number) => {
+    switch (value) {
+      case 1:
+        return 'Jan';
+      case 2:
+        return 'Feb';
+      case 3:
+        return 'Mar';
+      case 4:
+        return 'Apr';
+      case 5:
+        return 'May';
+      case 6:
+        return 'Jun';
+      case 7:
+        return 'Jul';
+      case 8:
+        return 'Aug';
+      case 9:
+        return 'Sep';
+      case 10:
+        return 'Oct';
+      case 11:
+        return 'Nov';
 
+      case 12:
+        return 'Dec';
+      default:
+        return;
+    }
+  };
+  const CHART1_DATA = chartData?.charts.map((data) => {
+    return {
+      name: monthGenerate(data.month),
+      Events: data.amount,
+    };
+  });
+  // console.log('CHART1', CHART1_DATA);
+  const CHART2_DATA = chartData?.charts.map((data) => {
+    return {
+      name: data.month,
+      Sales: data.profits,
+      Expenses: data.expenses,
+    };
+  });
+  // console.log('CHART2', CHART2_DATA);
+  // const CHART2_DATA = [
+  //   {
+  //     name: 'Jan',
+  //     Sales: 27,
+  //     Expenses: 10,
+  //   },
+  // ];
   return (
     <>
       <DashboardHeader {...{ setDateRange, fetchDashTotal }} />
@@ -250,35 +313,44 @@ const Dashboard = () => {
               <Typography fontWeight={500} variant='h5' ml={5} mb={3}>
                 Events
               </Typography>
-
-              <ResponsiveContainer width='100%' height='90%'>
-                <BarChart
-                  data={CHART1_DATA}
-                  barSize={25}
-                  layout={isMdDown ? 'vertical' : 'horizontal'}
+              {isLoadingChart ? (
+                <Stack
+                  sx={{ height: '100%' }}
+                  justifyContent='center'
+                  alignItems={'center'}
                 >
-                  <CartesianGrid vertical={false} />
+                  <CusLoading />
+                </Stack>
+              ) : (
+                <ResponsiveContainer width='100%' height='90%'>
+                  <BarChart
+                    data={CHART1_DATA}
+                    barSize={25}
+                    layout={isMdDown ? 'vertical' : 'horizontal'}
+                  >
+                    <CartesianGrid vertical={false} />
 
-                  {isMdDown ? (
-                    <>
-                      <XAxis type='number' domain={[0, 100]} />
-                      <YAxis dataKey='name' type='category' />
-                    </>
-                  ) : (
-                    <>
-                      <XAxis dataKey='name' />
-                      <YAxis domain={[0, 100]} />
-                    </>
-                  )}
+                    {isMdDown ? (
+                      <>
+                        <XAxis type='number' domain={[0, 100]} />
+                        <YAxis dataKey='name' type='category' />
+                      </>
+                    ) : (
+                      <>
+                        <XAxis dataKey='name' />
+                        <YAxis domain={[0, 100]} />
+                      </>
+                    )}
 
-                  <Tooltip />
-                  <Bar
-                    dataKey='Events'
-                    fill={theme.palette.primary.main}
-                    orientation='top'
-                  />
-                </BarChart>
-              </ResponsiveContainer>
+                    <Tooltip />
+                    <Bar
+                      dataKey='Events'
+                      fill={theme.palette.primary.main}
+                      orientation='top'
+                    />
+                  </BarChart>
+                </ResponsiveContainer>
+              )}
             </Paper>
 
             <Paper
@@ -286,6 +358,7 @@ const Dashboard = () => {
                 px: 3,
                 flexGrow: 1,
                 height: '100%',
+                overflow: 'auto',
               }}
             >
               <Typography
@@ -300,25 +373,40 @@ const Dashboard = () => {
               >
                 Upcoming Event
               </Typography>
-              <List
-                sx={{
-                  width: '100%',
-                  bgcolor: 'common.white',
-                  p: 0,
-                  overflow: 'auto',
-                }}
-              >
-                {Anniversary.map((data, i) => {
-                  return (
-                    <AnniversaryItem
-                      key={i}
-                      daysLeft={data.daysLeft}
-                      lastOrder={data.lastOrder}
-                      name={data.name}
-                    />
-                  );
-                })}
-              </List>
+              {chartData && chartData?.reminder.length > 0 ? (
+                <List
+                  sx={{
+                    bgcolor: 'common.white',
+                    p: 0,
+                  }}
+                >
+                  {chartData?.reminder.map((data, i) => {
+                    const s = moment(data.date, 'YYYY-MM-DD').format('DD');
+                    const e = moment(data.bookingDate, 'YYYY-MM-DD').format(
+                      'DD'
+                    );
+                    console.log('s:', s, 'e:', e);
+                    return (
+                      <AnniversaryItem
+                        key={i}
+                        daysLeft={data.id}
+                        lastOrder={data.bookingDate}
+                        name={data.customer.customer_name}
+                      />
+                    );
+                  })}
+                </List>
+              ) : (
+                <Stack
+                  direction={'column'}
+                  alignItems={'center'}
+                  justifyContent='center'
+                  sx={{ height: '60%' }}
+                >
+                  <BoxRemove size='64' color={theme.palette.primary.main} />
+                  <Typography variant='subtitle2'>No data</Typography>
+                </Stack>
+              )}
             </Paper>
           </Stack>
           <Stack
