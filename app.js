8770fdcf5fe45b8f5c13ae0a612afbd8021058f3
meassaominea <meassaominea@gmail.com>
2023-03-05 12:49:21 +0700

    build prod

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT;
+const port = 3001;
 
 app.use(express.static(path.join(__dirname, "build")));
 
